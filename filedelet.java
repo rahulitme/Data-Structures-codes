04e30ccdd1607b88961017fa78400ec3010e5da2
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-17 23:39:45 +0530

    Add files via upload

---
+++ filedelet.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+class filedelet {
+    public static void main(String[] args) {
+
+        // creates a file object
+        File file = new File("JavaFile.java");
+
+        // deletes the file
+        boolean value = file.delete();
+        if (value) {
+            System.out.println("JavaFile.java is successfully deleted.");
+        } else {
+            System.out.println("File doesn't exit");
+        }
+    }
+}
