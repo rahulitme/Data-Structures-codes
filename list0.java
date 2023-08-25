19d325d29497d2c6a0092f056894d6fdc025908d
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-25 20:02:32 +0530

    Add files via upload

---
+++ list0.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class list0 {
+    public static void main(String args[]) {
+
+        // Instantiating List using Arrays.asList()
+        List<Integer> list = Arrays.asList(1, 2, 3);
+
+        // Print the list
+        System.out.println("List : " + list.toString());
+    }
+}
