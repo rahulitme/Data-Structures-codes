227fe376d1dc0d8ba04a36f1596c9b2169a22e3d
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-02-10 05:28:48 +1200

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class ArrayString { 
+        public static void main(String[] args) {  
+            String[] strArray = { "0", "1", "Jjki" };  
+            
+            for (int i = 0; i < strArray.length; i++) 
+            {  
+             System.out.println(strArray[i]);
+            }
+             
+             }
+        }
+      
