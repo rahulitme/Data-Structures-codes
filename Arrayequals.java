fab52e8c968d17de79ead8b34167834c86e1af14
Rahul Kumar <118755562+rahulitme@users.noreply.github.com>
2024-02-14 06:02:50 +1200

    Add files via upload

---
+++ Arrayequals.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Arrayequals {
+public static void main(String args)
+{
+	int a[]= {1,2,3,4};
+	int b[]= {1,2,3,4};
+	boolean result=Arrayequals.equals(a, b);
+	if(result=true)
+	{
+		System.out.println("both array equlas");
+	}
+	else
+	{
+		System.out.println("both array are not equlas");
+		
+	}
+}
+
+}
